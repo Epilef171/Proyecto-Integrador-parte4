f51e5044b91d7b9b9614550e35e6410160b63aa5
Epilef171 <posadafelipe641@gmail.com>
2023-10-20 19:26:00 -0500

    Arreglos de codigo

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import os
+import keyboard
+import time
 from typing import List, Tuple
 
 def generate_maze(maze_str: str) -> List[List[str]]:
     return [list(row) for row in maze_str.strip().split("\n")]
-
-def string_to_matrix(maze_str: str, end: int) -> List[List[str]]:
-    maze_rows = maze_str.strip().split("\n")
-    maze_matrix = [list(row) for row in maze_rows]
-    for row in maze_matrix:
-        while len(row) < end:
-            row.append('#')
-    return maze_matrix
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -22,48 +16,60 @@ def display_matrix(maze: List[List[str]]):
 
 def main_loop(maze: List[List[str]], start: Tuple[int, int], end: Tuple[int, int]):
     px, py = start
+    end_x, end_y = end
 
-    while (px, py) != end:
+    while True:
         maze[py][px] = 'P'
         display_matrix(maze)
 
-        key = input("Presiona una tecla de flecha (Arriba, Abajo, Izquierda, Derecha): ").lower()
+        key = None
+        while key is None:
+            for direction in ['left', 'right', 'up', 'down']:
+                if keyboard.is_pressed(direction):
+                    key = direction
 
-        if key == "w" and py > 0 and maze[py - 1][px] != '#':
-            maze[py][px], py = '.', py - 1
-        elif key == "s" and py < len(maze) - 1 and maze[py + 1][px] != '#':
-            maze[py][px], py = '.', py + 1
-        elif key == "a" and px > 0 and maze[py][px - 1] != '#':
-            maze[py][px], px = '.', px - 1
-        elif key == "d" and px < len(maze[0]) - 1 and maze[py][px + 1] != '#':
-            maze[py][px], px = '.', px + 1
+        maze[py][px] = '.'
+        if key == 'left' and px > 0 and maze[py][px - 1] != '#':
+            px -= 1
+        elif key == 'right' and px < len(maze[0]) - 1 and maze[py][px + 1] != '#':
+            px += 1
+        elif key == 'up' and py > 0 and maze[py - 1][px] != '#':
+            py -= 1
+        elif key == 'down' and py < len(maze) - 1 and maze[py + 1][px] != '#':
+            py += 1
+
+        if (px, py) == (end_x, end_y):
+            print("¡Has llegado al final!")
+            break
+
+        time.sleep(0.2)  # Espera corta para evitar movimientos rápidos
 
 if __name__ == "__main__":
     maze_str = """..###################
-....................#
+#...................#
+#.###.#.###.#.#######
+#...#.#...#.#.....#.#
+#########.#.#.#.#.#.#
+#.#.#.....#.#.#.#...#
+#.#.###.###.###.#.###
+#.......#...#.#.#.#.#
 ###.###.#####.#####.#
-#...#...#.#.#.#.....#
-#####.#.#.#.#.#.###.#
-#.....#.#...#.#...#.#
-#.###.#.###.#.#####.#
-#.#...#.#.#...#.....#
-#.#######.#.#########
-#.....#...........#.#
-###.#####.#####.###.#
-#.#.#.#.......#.#...#
-#.#.#.###.#.#######.#
-#.......#.#.#.......#
-###.#.#####.#.#.###.#
-#...#.....#...#.#.#.#
-#.###########.#.#.###
-#.........#...#.#.#.#
-#.#.#.#####.#.###.#.#
-#.#.#...#...#.......
-###################."""
+#.#...#.............#
+#.#.###.###########.#
+#.....#.....#.....#.#
+#.###.###.###.###.#.#
+#.#.#...#...#...#...#
+#.#.#############.###
+#.#.......#.........#
+#.#.#.###.###.#.###.#
+#.#.#...#...#.#.#...#
+###.###.###.###.#.###
+#.....#...#.....#.###
+#################...."""
 
     maze_matrix = generate_maze(maze_str)
     
     start_position = (0, 0)
-    end_position = (22, 21)
+    end_position = (21, 21)
 
     main_loop(maze_matrix, start_position, end_position)
